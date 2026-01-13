9bbc234a7384b7ec2b84145d59913d2237e0dd59
Genaro Paoli <mgenasp2403@gmail.com>
2026-01-13 16:40:47 -0600

    Análisis de números y "limpieza de strings"

--- script.js
+++ script.js
@@ -4,3 +4,9 @@ const primerValor = prompt("Primer valor");
 let segudoValor = prompt("Indique el segundo valor");
 let tercerValor = prompt("Indique el segundo valor");
 
+//Analizar los números
+//Primero el prompt al ser un string se debe de pasar a un valor numérico
+
+const primvalNum = Number(primerValor);
+const segvalNum = Number(segudoValor);
+const tervalNum = Number(tercerValor);
