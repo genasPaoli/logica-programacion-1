86fa0ad4cf94782f1783357eb9addfcbcf77e962
Genaro Paoli <mgenasp2403@gmail.com>
2026-01-13 16:31:26 -0600

    Prompts para valores

---
+++ script.js
@@ -0,0 +1,6 @@
+// pedir un imput al usuario 
+
+const primerValor = prompt("Primer valor");
+let segudoValor = prompt("Indique el segundo valor");
+let tercerValor = prompt("Indique el segundo valor");
+
